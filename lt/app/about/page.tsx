a557decda2cd271948f18d58d5ca5b0a0dec758d
Ian Clavel <ianclavel1991@gmail.com>
2024-12-27 23:40:32 +0800

    add: gallery, about, contact pages

---
+++ lt/app/about/page.tsx
@@ -0,0 +1,20 @@
+import ResponsiveNavbar from "@/app/components/navbar/navbar";
+import ResponsiveFooter from "@/app/components/footer/footer";
+
+const Shop = () => {
+  return (
+    <>
+      <nav>
+        <ResponsiveNavbar />
+      </nav>
+      <main className="h-[100vh] w-full flex justify-center items-center">
+        SAMPLE ABOUT PAGE
+      </main>
+      <footer>
+        <ResponsiveFooter />
+      </footer>
+    </>
+  );
+};
+
+export default Shop;
