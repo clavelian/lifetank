bcf4f03c2150c45a3fa3371c715a50e2faa9b55f
Ian Clavel <ianclavel1991@gmail.com>
2024-12-28 22:40:25 +0800

    replaced blue border for a much simplier hover border and when active it will only highlight in white add: link for the logo

--- lt/app/components/navbar/navbar.tsx
+++ lt/app/components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/ltph.png";
 import Plant from "@/public/plant.svg";
@@ -11,22 +12,24 @@ const ResponsiveNavbar = () => {
 
   return (
     <nav className="flex items-center w-full rounded-full py-2 justify-center mobile:justify-evenly">
-      <Image src={Logo} alt="Logo" width={130} height={130} className="" />
+      <Link href="/">
+        <Image src={Logo} alt="Logo" width={130} height={130} className="" />
+      </Link>
 
       <ul className="flex justify-center items-center p-1 relative mobile:hidden font-semibold pl-14">
         <div
           className={`${
-            (activeTab === 0 && "-translate-y-[5rem]") ||
-            (activeTab === 1 && "translate-x-[-146px]") ||
-            (activeTab === 2 && "translate-x-[-50px]") ||
-            (activeTab === 3 && "translate-x-[41px]") ||
-            (activeTab === 4 && "!w-[100px] translate-x-[142px]")
-          } !bg-[#3B9DF8] absolute !text-[#fff] h-[85%] w-[95px] transition duration-500 rounded-full border-transparent cursor-pointer`}
+            (activeTab === 0 && "") || //scale-0
+            (activeTab === 1 && "") || //translate-x-[-146px]
+            (activeTab === 2 && "") || //translate-x-[-50px]
+            (activeTab === 3 && "") || //translate-x-[41px]
+            (activeTab === 4 && "!w-[1px]") // translate-x-[142px]
+          }absolute !text-[#fff] h-[85%] w-[95px] transition duration-500 rounded-full border-transparent cursor-pointer`} //!bg-[#3B9DF8]
         ></div>
         <li
           className={`${
             activeTab === 1 && " !text-[#fff]"
-          } px-6 py-2  text-[#ffffffc0] z-20 transition duration-300 rounded-full border-transparent cursor-pointer`}
+          } px-6 py-2 before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 text-[#ffffffc0] z-20   border-transparent cursor-pointer`}
           onClick={() => setActiveTab(1)}
         >
           Gallery
@@ -34,7 +37,7 @@ const ResponsiveNavbar = () => {
         <li
           className={`${
             activeTab === 2 && " !text-[#fff]"
-          } px-6 py-2  text-[#ffffffc0] z-20 transition duration-300 rounded-full border-transparent cursor-pointer`}
+          } px-6 py-2 before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 text-[#ffffffc0] z-20   border-transparent cursor-pointer`}
           onClick={() => setActiveTab(2)}
         >
           Shop
@@ -42,7 +45,7 @@ const ResponsiveNavbar = () => {
         <li
           className={`${
             activeTab === 3 && " !text-[#fff]"
-          } px-6 py-2  text-[#ffffffc0] z-20 transition duration-300 rounded-full border-transparent cursor-pointer`}
+          } px-6 py-2 before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 text-[#ffffffc0] z-20   border-transparent cursor-pointer`}
           onClick={() => setActiveTab(3)}
         >
           About
@@ -50,7 +53,7 @@ const ResponsiveNavbar = () => {
         <li
           className={`${
             activeTab === 4 && " !text-[#fff]"
-          } px-6 py-2  text-[#ffffffc0] z-20 transition duration-300 rounded-full border-transparent cursor-pointer`}
+          } px-6 py-2 before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 text-[#ffffffc0] z-20   border-transparent cursor-pointer`}
           onClick={() => setActiveTab(4)}
         >
           Contact
@@ -82,11 +85,14 @@ const ResponsiveNavbar = () => {
           <li className="hover:border-b-[#3B9DF8] border-b-[3px] border-transparent transition-all duration-300 cursor-pointer text-green-300">
             Gallery
           </li>
-          <li className="hover:border-b-[#3B9DF8] border-b-[2px] border-transparent transition-all duration-500 cursor-pointer text-green-300">
+          <li className="hover:border-b-[#3B9DF8] border-b-[3px] border-transparent transition-all duration-500 cursor-pointer text-green-300">
             Shop
           </li>
-          <li className="hover:border-b-[#3B9DF8] border-b-[2px] border-transparent transition-all duration-500 cursor-pointer text-green-300">
+          <li className="hover:border-b-[#3B9DF8] border-b-[3px] border-transparent transition-all duration-500 cursor-pointer text-green-300">
             About
+          </li>
+          <li className="hover:border-b-[#3B9DF8] border-b-[3px] border-transparent transition-all duration-500 cursor-pointer text-green-300">
+            Contact
           </li>
         </ul>
       </aside>
