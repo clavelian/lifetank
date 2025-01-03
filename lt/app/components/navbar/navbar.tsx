d89a81249d46b0a5948fc3524828428b2130c26c
Ian Clavel <ianclavel1991@gmail.com>
2025-01-04 04:17:30 +0800

    structure for Sheet

--- lt/app/components/navbar/navbar.tsx
+++ lt/app/components/navbar/navbar.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/ltph.png";
 import Plant from "@/public/plant.svg";
 
-const ResponsiveNavbar = () => {
-  const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
+const ResponsiveNavbar = () => {
   return (
-    <nav className="flex items-center py-2 justify-center mobile:justify-evenly">
+    <nav className="flex items-center py-4 justify-center mobile:justify-evenly">
       <Link href="/">
         <Image src={Logo} alt="Logo" width={130} height={130} className="" />
       </Link>
@@ -38,39 +44,30 @@ const ResponsiveNavbar = () => {
         </Link>
       </ul>
 
-      <Image
-        src={Plant}
-        alt="plant"
-        height={30}
-        width={30}
-        className="text-[1.9rem] mr-0 cursor-pointer exclude:hidden flex text-green-300"
-        onClick={() => setMobileSidebarOpen(!mobileSidebarOpen)}
-      />
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src={Plant}
+            alt="plant"
+            height={30}
+            width={30}
+            className="text-[1.9rem] mr-0 cursor-pointer exclude:hidden flex"
+          />
+        </SheetTrigger>
 
-      <aside
-        className={` ${
-          mobileSidebarOpen
-            ? "translate-x-0 opacity-100 z-20"
-            : "translate-x-0 opacity-0 z-[-1]"
-        } lg:hidden p-4 text-center absolute top-[65px] right-0 w-full rounded-md transition-all duration-300`}
-      >
-        <ul className="items-center gap-28 mobile:gap-[3.5rem] text-[1.2rem] flex justify-center pt-5">
-          <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#fafafa] transition-all duration-300 before:left-0 text-[#ffffffc0] cursor-pointer">
-            Gallery
-          </li>
-          <Link href="/shop">
-            <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#fafafa] transition-all duration-300 before:left-0 text-green-300 z-20   border-transparent cursor-pointer">
-              Shop
-            </li>
-          </Link>
-          <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#fafafa] transition-all duration-300 before:left-0 text-green-300 z-20   border-transparent cursor-pointer">
-            About
-          </li>
-          <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-[#fafafa] transition-all duration-300 before:left-0 text-green-300 z-20   border-transparent cursor-pointer">
-            Contact
-          </li>
-        </ul>
-      </aside>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle></SheetTitle>
+            <SheetDescription className="">
+              <ul className="font-semibold text-3xl py-[13rem]">
+                <li className="py-8">Get Started</li>
+                <li className="py-8">Shop</li>
+                <li className="py-8">Contact</li>
+              </ul>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </nav>
   );
 };
