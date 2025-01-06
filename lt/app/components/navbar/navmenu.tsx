c7a2a77ac4d733ee1e27e9cd5369f1de46c46edd
Ian Clavel <ianclavel1991@gmail.com>
2025-01-06 17:32:59 +0800

    recommit from previous commit to save

--- lt/app/components/navbar/navmenu.tsx
+++ lt/app/components/navbar/navmenu.tsx
@@ -81,15 +81,18 @@ export function NavMenu() {
           {/* contact */}
         </NavigationMenuList>
       </NavigationMenu>
-      <button className="relative inline-flex items-center justify-center px-4 py-1.5 overflow-hidden font-medium transition duration-300 ease-out border-2 rounded-lg group bg-slate-600">
-        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-slate-600 group-hover:translate-x-0 ease">
-          ❤️
-        </span>
-        <span className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform group-hover:translate-x-full ease font-bold text-sm text-white">
-          ᴄᴏɴᴛᴀᴄᴛ
-        </span>
-        <span className="relative invisible">ᴄᴏɴᴛᴀᴄᴛ</span>
-      </button>
+
+      <Link href="/contact">
+        <button className="relative inline-flex items-center justify-center px-4 py-1.5 overflow-hidden font-medium transition duration-300 ease-out border-2 rounded-lg group bg-slate-600">
+          <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-slate-600 group-hover:translate-x-0 ease">
+            ❤️
+          </span>
+          <span className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform group-hover:translate-x-full ease font-bold text-sm text-white">
+            ᴄᴏɴᴛᴀᴄᴛ
+          </span>
+          <span className="relative invisible">ᴄᴏɴᴛᴀᴄᴛ</span>
+        </button>
+      </Link>
     </>
   );
 }
