9dc1be746a13321f64250c1dff663125354a325c
Ian Clavel <ianclavel1991@gmail.com>
2025-01-10 09:32:16 +0800

    minor adjustment for hero

--- lt/app/components/hero/hero.tsx
+++ lt/app/components/hero/hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
         </video>
       </div>
       <div className="">
-        <span className="hero text-white absolute font-extrabold text-6xl mobile:top-[15rem] exclude:top-[18rem] wscreen:left-[13rem] smscreen:px-4 tablet:left-28 exclude:right-20 top-40 left-6 px-2 mobile:px-5 mobile:pr-3 smscreen:pl-24">
+        <span className="hero text-white absolute font-extrabold text-6xl mobile:top-[15rem] exclude:top-[18rem] wscreen:left-[12rem] smscreen:px-4 tablet:left-28 exclude:right-20 top-40 left-6 px-2 mobile:px-5 mobile:pr-3 smscreen:pl-24">
           Bring the serenity of nature to your desk.
         </span>
       </div>
