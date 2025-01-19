6dc37968ee698ae46a929243989478bea18f2463
Ian Clavel <ianclavel1991@gmail.com>
2025-01-19 23:46:43 +0800

    minor adjustments

--- lt/app/components/footer/footer.tsx
+++ lt/app/components/footer/footer.tsx
@@ -2,7 +2,7 @@ const ResponsiveFooter = () => {
   return (
     <footer className="bg-transparent boxShadow rounded-xl w-full p-6 exclude:p-9 absolute bottom-0 ">
       <div className="pt-[20px] flex items-center w-full flex-wrap gap-[1.2rem] justify-center">
-        <p className="text-[0.8rem] mobile:text-[0.5rem] text-gray-400">
+        <p className="text-[0.8rem] mobile:text-[.7rem] text-gray-400">
           © 2025 Lifetanks PH. All Rights Reserved.{" "}
         </p>
       </div>
