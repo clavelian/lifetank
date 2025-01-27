2ad068ebe046e3a2e156fe5141d46740737ac534
Ian Clavel <ianclavel1991@gmail.com>
2025-01-27 17:08:00 +0800

    minor change for floating dock menu

--- lt/components/ui/floating-dock.tsx
+++ lt/components/ui/floating-dock.tsx
@@ -65,7 +65,7 @@ const FloatingDockMobile = ({
                 <Link
                   href={item.href}
                   key={item.title}
-                  className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-900 flex items-center justify-center"
+                  className="h-10 w-10 rounded-full bg-transparent dark:bg-neutral-900 flex items-center justify-center"
                 >
                   <div className="h-4 w-4">{item.icon}</div>
                 </Link>
