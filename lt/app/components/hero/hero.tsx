d9c18317b15893ce812c5c467c777d0bf65d1b2d
Ian Clavel <ianclavel1991@gmail.com>
2025-01-27 20:06:09 +0800

    mobile breakpoint

--- lt/app/components/hero/hero.tsx
+++ lt/app/components/hero/hero.tsx
@@ -9,8 +9,9 @@ function Hero() {
           <source src="/1211.mp4" type="video/mp4" />
         </video>
       </div>
+      {/* TODO: sort all breakpoints */}
       <div className="">
-        <span className="hero text-white absolute font-extrabold text-6xl mobile:top-[13rem] exclude:top-[18rem] wscreen:left-[12rem] smscreen:px-4 tablet:left-28 exclude:right-20 top-40 left-6 px-2 mobile:px-3 mobile:pr-3 smscreen:pl-24 mobile:text-5xl wscreen:pl-[4.5rem] zxc:pl-[2.5rem] zxc:text-2xl zxc:top-[7rem]">
+        <span className="hero absolute text-white font-semibold mobile:text-6xl mobile:px-10 mobile:top-[30%]">
           Bring the serenity of nature to your desk.
         </span>
       </div>
@@ -23,3 +24,7 @@ function Hero() {
 }
 
 export default Hero;
+
+// text-white absolute font-extrabold text-6xl  exclude:top-[18rem] wscreen:left-[12rem] smscreen:px-4 tablet:left-28 exclude:right-20 top-40 left-6 px-2  smscreen:pl-24 mobile:text-5xl wscreen:pl-[4.5rem] zxc:pl-[2.5rem] zxc:text-2xl zxc:top-[7rem]
+
+// mobile:top-[13rem] mobile:px-3 mobile:pr-3
