ebb084f9590caa86a60315f711a64a34226d8289
Ian Clavel <ianclavel1991@gmail.com>
2025-01-29 00:27:30 +0800

    for testing

--- lt/app/page.tsx
+++ lt/app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/app/components/hero/hero";
 import ResponsiveFooter from "@/app/components/footer/footer";
+// import TestOne from "@/components/ui/test";
 
 export default function Home() {
   return (
     <>
       <main>
         <Hero />
+
+        {/* <div className="absolute top-0">
+          <TestOne />
+        </div> */}
       </main>
       <footer>
         <ResponsiveFooter />{" "}
