d66f47c2e7f08991c4271b969b0082fb58bf8337
Ian Clavel <ianclavel1991@gmail.com>
2025-01-29 23:14:07 +0800

    add: colourful-text

--- lt/components/ui/test.tsx
+++ lt/components/ui/test.tsx
@@ -1,6 +1,6 @@
 const TestOne = () => {
   return (
-    <h1 className="text-7xl text-red-600 flex items-center justify-center font-extrabold z-50">
+    <h1 className="text-7xl text-red-600 flex items-center justify-center font-extrabold z-5">
       HELLO WORLD
     </h1>
   );
