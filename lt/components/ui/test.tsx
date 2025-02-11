1a935838c025c019b75aeff83aa7f0f3a769e37a
Ian Clavel <ianclavel1991@gmail.com>
2025-02-11 22:11:48 +0800

    deleted test.tsx

--- lt/components/ui/test.tsx
+++
@@ -1,9 +0,0 @@
-const TestOne = () => {
-  return (
-    <h1 className="text-7xl text-red-600 flex items-center justify-center font-extrabold z-5">
-      HELLO WORLD
-    </h1>
-  );
-};
-
-export default TestOne;
