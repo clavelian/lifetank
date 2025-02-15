7506d2650ecac0d34affe1382606b56c2e578065
Ian Clavel <ianclavel1991@gmail.com>
2025-02-16 01:43:29 +0800

    added 3 more and edited the title

--- lt/app/shop/terrarium/page.tsx
+++ lt/app/shop/terrarium/page.tsx
@@ -10,6 +10,9 @@ import Six from "@/public/6.png";
 import Seven from "@/public/7.jpg";
 // import Eight from "@/public/8.jpg";
 import Nine from "@/public/9.jpg";
+import Ten from "@/public/10.jpg";
+import Eleven from "@/public/11.jpg";
+import Twelve from "@/public/12.jpg";
 import Link from "next/link";
 
 const Shop = () => {
@@ -125,7 +128,7 @@ const Shop = () => {
           {/*  text  */}
           <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
             <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
-              Mountainscape
+              Eternal
             </h1>
             <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -157,7 +160,7 @@ const Shop = () => {
           {/*  text  */}
           <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
             <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
-              Mountainscape
+              Miniscape
             </h1>
             <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -189,7 +192,7 @@ const Shop = () => {
           {/*  text  */}
           <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
             <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
-              Mountainscape
+              Spring
             </h1>
             <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -221,7 +224,7 @@ const Shop = () => {
           {/*  text  */}
           <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
             <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
-              Mountainscape
+              Bloom
             </h1>
             <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -253,7 +256,102 @@ const Shop = () => {
           {/*  text  */}
           <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
             <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
-              Mountainscape
+              Celestial
+            </h1>
+            <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+            <Link href="/shop/wildflower">
+              <span className="bg-gray-400 z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 px-3 py-2 mt-3 hover:bg-gray-500 transition-all duration-1000 text-white rounded-md text-[0.9rem]">
+                View Details
+              </span>
+            </Link>
+          </div>
+
+          {/*  bottom shadow  */}
+          <div className="w-full opacity-0 z-[-1] group-hover:opacity-100 group-hover:z-10 transition-all duration-500 bg-gradient-to-b from-[rgb(0,0,0,0.001)] to-[rgb(0,0,0,0.5)] h-[100%] absolute bottom-0 left-0 right-0"></div>
+        </div>
+        {/* end */}
+        {/* Sibuyan */}
+        <div className="w-full flex mobile:w-[10rem] exclude:w-[20%] mobile:h-[15rem] h-[25rem] relative overflow-hidden group cursor-pointer rounded-md">
+          {/*  image  */}
+          <Image
+            src={Ten}
+            alt="Ten"
+            height={1000}
+            width={1000}
+            className="w-full h-full object-cover group-hover:scale-[1.1] transition-all duration-700"
+          />
+
+          {/*  text  */}
+          <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
+            <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
+              Sibuyan
+            </h1>
+            <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+            <Link href="/shop/wildflower">
+              <span className="bg-gray-400 z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 px-3 py-2 mt-3 hover:bg-gray-500 transition-all duration-1000 text-white rounded-md text-[0.9rem]">
+                View Details
+              </span>
+            </Link>
+          </div>
+
+          {/*  bottom shadow  */}
+          <div className="w-full opacity-0 z-[-1] group-hover:opacity-100 group-hover:z-10 transition-all duration-500 bg-gradient-to-b from-[rgb(0,0,0,0.001)] to-[rgb(0,0,0,0.5)] h-[100%] absolute bottom-0 left-0 right-0"></div>
+        </div>
+        {/* end */}
+
+        {/* Pinecone */}
+        <div className="w-full flex mobile:w-[10rem] exclude:w-[20%] mobile:h-[15rem] h-[25rem] relative overflow-hidden group cursor-pointer rounded-md">
+          {/*  image  */}
+          <Image
+            src={Eleven}
+            alt="Eleven"
+            height={1000}
+            width={1000}
+            className="w-full h-full object-cover group-hover:scale-[1.1] transition-all duration-700"
+          />
+
+          {/*  text  */}
+          <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
+            <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
+              Pinecone
+            </h1>
+            <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+            <Link href="/shop/wildflower">
+              <span className="bg-gray-400 z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 px-3 py-2 mt-3 hover:bg-gray-500 transition-all duration-1000 text-white rounded-md text-[0.9rem]">
+                View Details
+              </span>
+            </Link>
+          </div>
+
+          {/*  bottom shadow  */}
+          <div className="w-full opacity-0 z-[-1] group-hover:opacity-100 group-hover:z-10 transition-all duration-500 bg-gradient-to-b from-[rgb(0,0,0,0.001)] to-[rgb(0,0,0,0.5)] h-[100%] absolute bottom-0 left-0 right-0"></div>
+        </div>
+        {/* end */}
+
+        {/* Sulu */}
+        <div className="w-full flex mobile:w-[10rem] exclude:w-[20%] mobile:h-[15rem] h-[25rem] relative overflow-hidden group cursor-pointer rounded-md">
+          {/*  image  */}
+          <Image
+            src={Twelve}
+            alt="Twelve"
+            height={1000}
+            width={1000}
+            className="w-full h-full object-cover group-hover:scale-[1.1] transition-all duration-700"
+          />
+
+          {/*  text  */}
+          <div className="absolute top-[54%] transform group-hover:translate-y-[-50%] transition-all duration-500 w-full h-full left-0 z-20 right-0 flex items-center justify-center flex-col">
+            <h1 className="text-[1.5rem] mobile:text-[.5rem] font-extrabold text-white text-center capitalize shopTitle">
+              Sulu
             </h1>
             <p className="text-center z-[1-] opacity-0 group-hover:z-20 group-hover:opacity-100 transition-all duration-700 text-white text-[0.9rem] pb-3">
               Lorem Ipsum is simply dummy text of the printing and typesetting
